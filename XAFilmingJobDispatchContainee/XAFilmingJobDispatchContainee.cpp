1b58949cef3ff1c84028b1412777a0383e1184ca
whhub <wanghui122004@me.com>
2017-09-22 17:40:48 +0800

    [XAFilming][013] [XAFilmingJobDispatchContainee] Startup DoWork

--- XAFilmingJobDispatchContainee/XAFilmingJobDispatchContainee.cpp
+++ XAFilmingJobDispatchContainee/XAFilmingJobDispatchContainee.cpp
@@ -1,15 +1,28 @@
 #include "XAFilmingJobDispatchContainee.h"
 #include "XAFilmingLogger.h"
+#include <McsfContainee/mcsf_containee_cmd_id.h>
 
 
 void XAFilmingJobDispatchContainee::Startup()
 {
 	LOG_INFO_XA_FILMING << "Startup" << "\n";
+
+	if (nullptr == m_pCommunicationProxy)
+	{
+		LOG_WARN_XA_FILMING << "the pProxy is null for the containee" << "\n";
+		return;
+	}
+
 }
 
 void XAFilmingJobDispatchContainee::DoWork()
 {
 	LOG_INFO_XA_FILMING << "DoWork" << "\n";
+
+	auto send_system_event_result = m_pCommunicationProxy->SendSystemEvent( "", static_cast<int>(SYSTEM_COMMAND_EVENT_ID_COMPONENT_READY), m_pCommunicationProxy->GetName() );
+	send_system_event_result ?
+			LOG_ERROR_XA_FILMING << "Fail to send componet_ready event to System manager,Please restart the containee" << "\n"
+		:	LOG_ERROR_XA_FILMING << "Succeed to send componet_ready event to System manager" << "\n";
 }
 
 bool XAFilmingJobDispatchContainee::Shutdown(bool bReboot)
