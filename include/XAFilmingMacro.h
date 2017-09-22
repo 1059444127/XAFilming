f966f09773f8f59a08ebee066a8463ac2e699bc0
whhub <wanghui122004@me.com>
2017-09-22 15:45:19 +0800

    [XAFilming][009] [XAFilmingWrapper] XAFilmingLogger XAFilmingMacro (禁用复制和拷贝构造函数，命名空间 宏定义)

---
+++ include/XAFilmingMacro.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define XA_FILMING_DISALLOW_COPY(classname) \
+    classname(const classname & );
+#define XA_FILMING_DISALLOW_ASSIGN(classname) \
+    void operator = (const classname & );
+#define XA_FILMING_DISALLOW_COPY_AND_ASSIGN(classname) \
+    XA_FILMING_DISALLOW_COPY  (classname)\
+    XA_FILMING_DISALLOW_ASSIGN(classname)
+
+
+#define MCSF_NAMESPACE_FOR_XA       Mcsf
