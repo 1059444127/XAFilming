fdd59f5fedb0c1fa70e82de8dac04a32bb59086b
whhub <wanghui122004@me.com>
2017-09-25 20:04:06 +0800

    [XAFilming][022] [Locally Run] Command Sender Mock

---
+++ Filming.CommandSenderSimulator/FilmingCommandSenderSimulatorContainee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UIH.Mcsf.Core;
+using UIH.XA.GlobalParameter;
+
+namespace UIH.XA.Filming.CommandSenderSimulator
+{
+    public class FilmingCommandSenderSimulatorContainee : CLRContaineeBase
+    {
+        private Dictionary<int, Action<int>> _actionDictionary = new Dictionary<int, Action<int>>();
+
+        /// <inheritdoc />
+        public override void Startup()
+        {
+            Console.WriteLine("StartUp");
+            _actionDictionary.Add(CommunicationCommandID.COMMAND_ID_PRINT, SendPrintCommand);
+        }
+
+
+        /// <inheritdoc />
+        public override void DoWork()
+        {
+            if (0 != SendSystemEvent("", (int)CLRContaineeEventId.SYSTEM_COMMAND_EVENT_ID_COMPONENT_READY, GetCommunicationProxy().GetName()))
+            {
+                Console.WriteLine("The event send to System manager fail,Please restart the FilmingFEContainee");
+            }
+            Console.WriteLine("has informed system manager that FilmingViewer is up");
+
+            while (true)
+            {
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+
+                SendCommand(CommunicationCommandID.COMMAND_ID_PRINT);
+            }
+        }
+
+        private void SendCommand(int commandID)
+        {
+            if(_actionDictionary.ContainsKey(commandID)) _actionDictionary[commandID].Invoke(commandID);
+            else
+                Console.WriteLine("Non Action for command [{0}]", commandID);
+        }
+
+
+        private void SendPrintCommand(int commandID)
+        {
+            var commandText = new CommandContext();
+            commandText.iCommandId = commandID;
+            commandText.sReceiver = "XAFilmingService@@";
+            commandText.sStringObject = @"E:\1.dcm";
+            commandText.pCommandCallback = new PrintCommandCallBack();
+
+            var proxy = GetCommunicationProxy();
+            var result = proxy.AsyncSendCommand(commandText);
+            Console.WriteLine("Command Result : [{0}]", result);
+        }
+    }
+}
---
+++ Filming.CommandSenderSimulator/PrintCommandCallBack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UIH.Mcsf.Core;
+using IAsyncResult = UIH.Mcsf.Core.IAsyncResult;
+
+namespace UIH.XA.Filming.CommandSenderSimulator
+{
+    class PrintCommandCallBack : ICommandCallbackHandler
+    {
+        /// <inheritdoc />
+        public override int HandleReply(IAsyncResult pAsyncResult)
+        {
+            Console.WriteLine("Reply is : [{0}]", pAsyncResult.GetStringObject());
+            return base.HandleReply(pAsyncResult);
+        }
+    }
+}
