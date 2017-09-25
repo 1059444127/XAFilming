9efe6674b437a65261779f1992d7f331b5837d3a
whhub <wanghui122004@me.com>
2017-09-25 14:11:18 +0800

    [XAFilming][018] Add Project XAFilmingContaineeTemplate

---
+++ XAFilmingContaineeTemplate/XAFilmingSampleContainee.cpp
@@ -0,0 +1,73 @@
+#include "XAFilmingSampleContainee.h"
+#include "XAFilmingLogger.h"
+#include <McsfContainee/mcsf_containee_cmd_id.h>
+
+
+IMPLEMENT_CONTAINEE(XAFilmingSampleContainee);
+
+void XAFilmingSampleContainee::Startup()
+{
+	LOG_INFO_XA_FILMING << "Startup" << LOG_END;
+
+	if (nullptr == m_pCommunicationProxy)
+	{
+		LOG_WARN_XA_FILMING << "the pProxy is null for the containee" << LOG_END;
+		return;
+	}
+
+}
+
+void XAFilmingSampleContainee::DoWork()
+{
+	LOG_INFO_XA_FILMING << "DoWork" << LOG_END;
+
+	auto send_system_event_result = m_pCommunicationProxy->SendSystemEvent( "", static_cast<int>(SYSTEM_COMMAND_EVENT_ID_COMPONENT_READY), m_pCommunicationProxy->GetName() );
+	send_system_event_result ?
+			LOG_ERROR_XA_FILMING << "Fail to send componet_ready event to System manager,Please restart the containee" << LOG_END
+		:	LOG_ERROR_XA_FILMING << "Succeed to send componet_ready event to System manager" << LOG_END;
+}
+
+bool XAFilmingSampleContainee::Shutdown(bool bReboot)
+{
+	LOG_INFO_XA_FILMING << "Shutdown with message bReboot ? [" << bReboot  << "]\n";
+	return true;
+}
+
+void XAFilmingSampleContainee::SetCommunicationProxy(MCSF_NAMESPACE_FOR_XA::ICommunicationProxy* pProxy)
+{
+	LOG_INFO_XA_FILMING << "SetCommunicationProxy" << LOG_END;
+	m_pCommunicationProxy = pProxy;
+}
+
+int XAFilmingSampleContainee::GetEstimatedTimeToFinishJob(bool bReboot)
+{
+	LOG_INFO_XA_FILMING << "GetEstimatedTmeToFinishJob with message bReboot ? [" << bReboot  << "]\n";
+	return 0;
+}
+
+void XAFilmingSampleContainee::SetCustomConfigFile(const std::string& sFilename)
+{
+	LOG_INFO_XA_FILMING << "CustomConfigFile: "  <<  sFilename << LOG_END;
+}
+
+std::list<std::string> XAFilmingSampleContainee::GetRunningTasks()
+{
+	return std::list<std::string>();
+}
+
+void XAFilmingSampleContainee::StartShutdown(bool bReboot)
+{
+	LOG_INFO_XA_FILMING << "bReboot: "  <<  bReboot << LOG_END;
+}
+
+int XAFilmingSampleContainee::GetTaskRemainingProgress(std::list<TaskProgress> & taskProgress)
+{
+	LOG_INFO_XA_FILMING << "taskProgress.Count = "  <<  taskProgress.size() << LOG_END;
+	return 0;
+}
+
+	XAFilmingSampleContainee::~XAFilmingSampleContainee()
+{
+	LOG_INFO_XA_FILMING << "Destructor" << LOG_END;
+}
+
---
+++ XAFilmingContaineeTemplate/XAFilmingSampleContainee.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "XAFilmingMacro.h"
+#include "McsfContainee\mcsf_containee_base.h"
+using namespace MCSF_NAMESPACE_FOR_XA;
+
+class ICommunicationProxy;
+class IEventHandler;
+class ICommandHandler;
+class IBigDataHandler;
+
+
+class XAFilmingSampleContainee : public IContainee
+{
+public:
+
+
+	XAFilmingSampleContainee(): m_pCommunicationProxy(nullptr)
+	{
+	}
+
+	/**
+	 * \brief Containee Startup
+	 */
+	void Startup() ;
+	/**
+	 * \brief Containee DoWork
+	 */
+	void DoWork() ;
+	/**
+	 * \brief Containee Shutdown
+	 * \param bReboot if reboot or not
+	 * \return ?
+	 */
+	bool Shutdown(bool bReboot) ;
+	///**
+	// * \brief 
+	// * \param pProxy CommunicationProxy Created by Container
+	// */
+	void SetCommunicationProxy(MCSF_NAMESPACE_FOR_XA::ICommunicationProxy* pProxy) ;
+	/**
+	 * \brief Estimate time to finish job for waiting system manager
+	 * \param bReboot if reboot or not
+	 * \return time left to finish job , unit: ms
+	 */
+	virtual int GetEstimatedTimeToFinishJob(bool bReboot);
+
+	virtual void SetCustomConfigFile(const std::string& sFilename);
+	virtual std::list<std::string> GetRunningTasks();
+	virtual void StartShutdown(bool bReboot);
+	virtual int GetTaskRemainingProgress(std::list<TaskProgress>& taskProgress);
+	virtual ~XAFilmingSampleContainee();
+
+private:
+	XA_FILMING_DISALLOW_COPY_AND_ASSIGN(XAFilmingSampleContainee)
+	MCSF_NAMESPACE_FOR_XA::ICommunicationProxy * m_pCommunicationProxy;
+};
+
+DECLARE_CONTAINEE(XAFilmingJobDispatchContainee);
