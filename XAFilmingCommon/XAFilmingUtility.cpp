1ed33860f65598d8464d77f238d059766a78ee4c
whhub <wanghui122004@me.com>
2017-09-28 17:56:17 +0800

    [XAFilming][031] [XAFimingCommon] [XAFilmingUtility] Using log

--- XAFilmingCommon/XAFilmingUtility.cpp
+++ XAFilmingCommon/XAFilmingUtility.cpp
@@ -1,7 +1,11 @@
 #include "XAFilmingUtility.h"
+#include "XAFilmingLogger.h"
+
 
 void split(const string serializedString, const string separator, vector<string> & splitStringVector)
 {
+	LOG_INFO_XA_FILMING << "Spliting [" << serializedString << "] with separator [" << separator << "]" << LOG_END;
+
 	size_t begin = 0;
 	size_t end = serializedString.find(separator, begin);
 		
@@ -20,13 +24,19 @@ void split(const string serializedString, const string separator, vector<string>
 
 void join(const vector<string>& stringVector, const string separator, string& joinedString)
 {
+	LOG_INFO_XA_FILMING << "Joinning series with separator [" << separator << "]" << LOG_END;
 }
 
 void serializeStrings(const vector<string>& stringVector, string& serializedString)
 {
+	LOG_INFO_XA_FILMING << "serializing strings " << LOG_END;
+	join(stringVector, "|", serializedString);
 }
 
 void deserializeStrings(const string serializedString, vector<string>& splitStringVector)
 {
+	LOG_INFO_XA_FILMING << "Deserializing [" << serializedString << "]" <<  LOG_END;
 	split(serializedString, "|", splitStringVector);
+
+	splitStringVector.push_back("OneString");
 }
--- XAFilmingCommon/XAFilmingUtility.h
+++ XAFilmingCommon/XAFilmingUtility.h
@@ -3,7 +3,7 @@
 #include "XAFilmingMacro.h"
 
 using namespace  std;
-
+	
 XA_FilmingCommon_Export void serializeStrings(const vector<string>& stringVector, string& serializedString);
 XA_FilmingCommon_Export void deserializeStrings(const string serializedString, vector<string>& splitStringVector);
 
