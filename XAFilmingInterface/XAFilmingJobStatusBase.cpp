722c15d12ef278f83168c0c4fcf6298646ce9f89
whhub <wanghui122004@me.com>
2017-10-05 16:55:27 +0800

    [XAFilming][043] Add Project XAFilmingJobStatus

--- XAFilmingInterface/XAFilmingJobStatusBase.cpp
+++ XAFilmingInterface/XAFilmingJobStatusBase.cpp
@@ -5,14 +5,17 @@
 void XAFilmingJobStatusBase::Continue(IXAFilmingJob*  const job)
 {
     LOG_WARN_XA_FILMING << "Continue job [ " << job->GetJobID() << "] with Status [" << job->GetJobStatus()->ToString() << "]" << LOG_END;
+    LOG_WARN_XA_FILMING << "Job Status: " << ToString() << LOG_END;
 }
 
 void XAFilmingJobStatusBase::Pause(IXAFilmingJob*  const job)
 {
     LOG_WARN_XA_FILMING << "Pause job [ " << job->GetJobID() << "] with Status [" << job->GetJobStatus()->ToString() << "]" << LOG_END;
+    LOG_WARN_XA_FILMING << "Job Status: " << ToString() << LOG_END;
 }
 
 void XAFilmingJobStatusBase::Restart(IXAFilmingJob*  const job)
 {
     LOG_WARN_XA_FILMING << "Restart job [ " << job->GetJobID() << "] with Status [" << job->GetJobStatus()->ToString() << "]" << LOG_END;
+    LOG_WARN_XA_FILMING << "Job Status: " << ToString() << LOG_END;
 }
