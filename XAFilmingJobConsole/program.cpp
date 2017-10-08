7bd0acc8e67b49b73dfb05e78d563078795d86a7
whhub <wanghui122004@me.com>
2017-10-08 13:42:56 +0800

    [XAFilming][079][XAFilmingJobConsole] Usage

--- XAFilmingJobConsole/program.cpp
+++ XAFilmingJobConsole/program.cpp
@@ -1,9 +1,124 @@
 #include <iostream>
+#include "IXAFilmingJobDispatch.h"
+#include <sstream>
+#include <map>
 using namespace std;
+
+IXAFilmingJobDispatch* job_dispatcher;
+
+void addJob(const vector<int> IDs)
+{
+    job_dispatcher->AddJob(nullptr);
+}
+
+void continueJobs(const vector<int> IDs)
+{
+    job_dispatcher->ContinueJobs(IDs);
+}
+
+void deleteJobs(const vector<int> IDs)
+{
+    job_dispatcher->DeleteJobs(IDs);
+}
+
+void pauseJobs(const vector<int> IDs)
+{
+    job_dispatcher->PauseJobs(IDs);
+}
+
+void showStatus(const vector<int> IDs)
+{
+    job_dispatcher->PushJobsProgress();
+}
+
+void restartJobs(const vector<int> IDs)
+{
+    job_dispatcher->RestartJobs(IDs);
+}
+
+void urgentJobs(const vector<int> IDs)
+{
+    job_dispatcher->UrgentJobs(IDs);
+}
+
+typedef void (*Execute)(const vector<int> parameters);
+map<string, Execute> command_map;
+
+
+void printUsage()
+{
+    cout << "Operation List" << endl;
+    cout << "\t" << "addJob" << endl;
+    cout << "\t" << "continueJobs 1 2 3" << endl;
+    cout << "\t" << "deleteJobs 1 2 3" << endl;
+    cout << "\t" << "pauseJobs 1 2 3" << endl;
+    cout << "\t" << "status" << endl;
+    cout << "\t" << "restartJobs 1 2 3" <<endl;
+    cout << "\t" << "urgentJobs 1 2 3" << endl;
+}
+
+
+vector<int> splitParameters(stringstream& ss)
+{
+    vector<int> parameters;
+    while(!ss.eof())
+    {
+        int i;
+        ss >> i;
+        parameters.push_back(i);
+    }
+    return parameters;
+}
+
+void ExcuteCommand(string str)
+{
+    stringstream ss;
+    ss << str;
+    string commandName;
+    ss >> commandName;
+
+    vector<int> parameters = splitParameters(ss);
+
+    auto command = command_map[commandName];
+    command(parameters);
+}
+
+void LauguageTest()
+{
+    char* s = "addJob 1 2 3";
+    stringstream ss;
+    string commandName;
+    ss << s;
+    ss >> commandName;
+    cout << commandName << endl;
+
+    cout << ss.str() << endl;
+
+    cout << "string(\"123\") == \"123\""  << endl;
+    cout << (string("123") == "123") << endl;
+
+
+    cout << "input a character to continue" << endl;
+    char c;
+    cin >> c;
+}
 
 void main()
 {
-    cout << "Input a charactor to continue" << endl;
-    char c;
-    cin >> c;
+    command_map["addJob"] = addJob;
+    command_map["continueJobs"] = continueJobs;
+    command_map["deleteJobs"] = deleteJobs;
+    command_map["pauseJobs"] = pauseJobs;
+    command_map["status"] = showStatus;
+    command_map["restartJobs"] = restartJobs;
+    command_map["urgentJobs"] = urgentJobs;
+
+
+    while(true)
+    {
+        printUsage();
+        char command[100];
+        cin.getline(command, 100);
+        ExcuteCommand(command);
+    }
 }
