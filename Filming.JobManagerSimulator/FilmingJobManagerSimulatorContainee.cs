94c6c48338ac41e4c224fd6e3828929132369d40
whhub <wanghui122004@me.com>
2017-10-12 10:08:29 +0800

    [XAFilming][0126][JobManagerSimulator] Start a Window in a Thread

--- Filming.JobManagerSimulator/FilmingJobManagerSimulatorContainee.cs
+++ Filming.JobManagerSimulator/FilmingJobManagerSimulatorContainee.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Windows.Threading;
 using UIH.Mcsf.Core;
 
 namespace UIH.XA.Filming.JobManagerSimulator
@@ -9,12 +11,24 @@ namespace UIH.XA.Filming.JobManagerSimulator
         public override void Startup()
         {
             Console.WriteLine("StartUp");
+
+
+            var thread = new Thread(ShowWindow);
+            thread.SetApartmentState(ApartmentState.STA);   //UI thread must be STA
+            thread.Start();
+        }
+
+        private void ShowWindow()
+        {
+            var window = new JobManagerWindow();
+            window.ShowDialog();                            //Should Be ShowDialog, not Show
         }
 
 
         /// <inheritdoc />
         public override void DoWork()
         {
+            Console.WriteLine("DoWork");
             if (0 != SendSystemEvent("", (int)CLRContaineeEventId.SYSTEM_COMMAND_EVENT_ID_COMPONENT_READY, GetCommunicationProxy().GetName()))
             {
                 Console.WriteLine("The event send to System manager fail,Please restart the FilmingFEContainee");
---
+++ Filming.JobManagerSimulator/JobManagerWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace UIH.XA.Filming.JobManagerSimulator
+{
+    /// <summary>
+    /// Interaction logic for JobManagerWindow.xaml
+    /// </summary>
+    public partial class JobManagerWindow
+    {
+        public JobManagerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
