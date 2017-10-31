219aaf76f20fed1aba207450f4812fc2b628f5a3
whhub <wanghui122004@me.com>
2017-10-31 15:36:54 +0800

    [XAFilming][167][XAFilming] XAFilming as PrintResultSubject

--- XAFilming/XAFilming.cpp
+++ XAFilming/XAFilming.cpp
@@ -1,9 +1,25 @@
 #include "XAFilming.h"
+#include "XAFilmingLogger.h"
+#include <XAPrint.h>
 
 void XAFilming::Print(const std::vector<std::string>& filePaths)
 {
+	bool bResult = MCSF_NAMESPACE_FOR_XA::print(filePaths);
+	LOG_INFO_XA_FILMING << "Print Success ? " << bResult << LOG_END;
+
+	PublishPrintResult(bResult);
 }
 
 void XAFilming::Register(IPrintResultObserver* pObserver)
 {
+	LOG_INFO_XA_FILMING << "Register a printResultObserver " << LOG_END;
+	_observers.push_back(pObserver);
+}
+
+void XAFilming::PublishPrintResult(bool bResult)
+{
+	for(auto iter = _observers.begin(); iter != _observers.end(); ++iter)
+	{
+		(*iter)->NofifyPrintStatus(bResult);
+	}
 }
--- XAFilming/XAFilming.h
+++ XAFilming/XAFilming.h
@@ -8,4 +8,7 @@ class XA_Filming_Export XAFilming : IXAFilming, IPrintResultSubject
 public:
 	virtual void Print(const std::vector<std::string>& filePaths);
 	virtual void Register(IPrintResultObserver* pObserver);
+private:
+	std::vector<IPrintResultObserver*> _observers;
+	void PublishPrintResult(bool bResult);
 };
