0cc1fe1b22fcdb746a0df26e68096ba29174aeab
whhub <wanghui122004@me.com>
2017-11-03 17:43:21 +0800

    [XAFilming][179] Add project XAFilmingWrapper_UT

---
+++ XAFilmingWrapper_UT/PrinterConfigTests.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "gtest/gtest.h"
+
+/// \class PrinterConfigTests
+/// \brief PrinterConfig Test suite class 
+///
+class PrinterConfigTests : public testing::Test
+{
+
+};
+
---
+++ XAFilmingWrapper_UT/XAFilmingWrapperUT.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <gtest/gtest.h>
+using namespace std;
+
+//xml output file of UT result 
+static const string ksTestOutputFile = "xml:XAFilmingWrapperUT_result.xml";
+
+int main(int argc, char* argv[])
+{
+	testing::GTEST_FLAG(output)=ksTestOutputFile;
+	testing::InitGoogleTest(&argc, argv);  
+
+	(void)RUN_ALL_TESTS();
+
+	cout << "Press Any Key to continue" << endl;
+	char c;
+	cin  >> c;
+
+	return 0;
+}
