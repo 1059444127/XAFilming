4fdc1042bd36e1abebf96a28c27f601e998e039a
whhub <wanghui122004@me.com>
2017-11-08 14:54:21 +0800

    [XAFilming][190][XAFilmingWrapper] Finish // TODO-Film-MayBe: 私有 Tag 添加任务信息

--- Filming.Proxy/FilmingProxy.cs
+++ Filming.Proxy/FilmingProxy.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.Composition;
+using System.Text;
+using UIH.Mcsf.Database;
 using UIH.Mcsf.Pipeline.Data;
 using UIH.XA.Core;
 using UIH.XA.Filming.Interface;
@@ -11,6 +13,8 @@ namespace UIH.XA.Filming.Proxy
     public class FilmingProxy : IFilming
     {
         [Import] private ICommunicator _communicator;
+
+        private const uint PacketHeaderTag = 0X00613102;
 
         public void Print(params string[] paths)
         {
@@ -34,15 +38,26 @@ namespace UIH.XA.Filming.Proxy
             try
             {
                 this.LogDevInfo("Begin to print DataHeaders");
+
+                var uidManager = McsfDatabaseDicomUIDManagerFactory.Instance().CreateUIDManager();
+                var jobID = uidManager.CreateSeriesUID(string.Empty);
+
                 int dataHeaderIndex = 0;
+                int dataHeaderCount = dataHeaders.Length;
                 foreach (var dataHeader in dataHeaders)
                 {
-                    // TODO-Film-MayBe: 私有 Tag 添加任务信息
+                    string packetHeader = string.Join(CommunicationCommandID.ParameterListSeparator, jobID,
+                        dataHeaderIndex.ToString(), dataHeaderCount.ToString());
+                    var packetHeaderBytes = Encoding.UTF8.GetBytes(packetHeader);
+
+                    dataHeader.InsertElement(PacketHeaderTag, packetHeaderBytes);
+
+
                     byte[] serializedObject;
                     if (!dataHeader.Serialize(out serializedObject))
                     {
-                        this.LogDevInfo(string.Format("Fail to serialize dataHeader [{0}]", dataHeaderIndex++));
-                        continue;
+                        this.LogDevError(string.Format("Fail to serialize dataHeader [{0}]", dataHeaderIndex));
+                        return;
                     }
 
                     this.LogDevInfo(string.Format("Succeed to seriealize dataHeader [{0}]", dataHeaderIndex++));
@@ -56,4 +71,36 @@ namespace UIH.XA.Filming.Proxy
             }
         }
     }
+
+    static class DicomAttributeCollectionExtension
+    {
+        public static void InsertElement(this DicomAttributeCollection dataHeader, uint tag, byte[] value)
+        {
+            var type = typeof(DicomAttributeCollectionExtension);
+
+            if (dataHeader.Contains(tag))
+            {
+                dataHeader.RemoveDicomAttribute(tag);
+                type.LogDevInfo(string.Format("Removed old element with tag [{0}]", tag));
+            }
+            var element = DicomAttribute.CreateAttribute(tag);
+            if (null == element)
+            {
+                type.LogDevWarning(string.Format("Failed to Create Dicom Attribute for tag [{0}] ",
+                    tag));
+                return;
+            }
+            if (!element.SetBytes(0, value))
+            {
+                type.LogDevWarning(
+                    string.Format("Failed to insert element [{0}] with tag [{1}]", value, tag));
+                return;
+            }
+
+            type.LogDevInfo(
+                string.Format("Succeeded to insert element [{0}] with tag [{1}]", value, tag));
+        }
+    }
 }
+
+
