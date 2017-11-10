d04a0f17065563bb46c116770df41ad17cb79ada
whhub <wanghui122004@me.com>
2017-11-10 10:41:14 +0800

    [XAFilming][208][XAFilmingWrapper] Correct XANow

--- XAFilmingWrapper/XABoost.cpp
+++ XAFilmingWrapper/XABoost.cpp
@@ -9,8 +9,8 @@ std::string XANow()
     // 这时候strTime里存放时间的格式是YYYYMMDDTHHMMSS，日期和时间用大写字母T隔开了    
     
 	//TODO-MayBe: Replace XANow algorithm with year month day hour minute seconds composition
-	strTime.replace(2,0,std::string("-"));
-	strTime.replace(5,0,std::string("-"));
+	strTime.replace(4,0,std::string("-"));
+	strTime.replace(7,0,std::string("-"));
 	
     int pos = strTime.find('T');    
     strTime.replace(pos,1,std::string("/"));    
